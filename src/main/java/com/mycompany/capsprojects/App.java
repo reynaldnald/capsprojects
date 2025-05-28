924085d11983487bf4ff4d87d0c954e583051ea1
Markuuu13 <markjuscardeno13@gmail.com>
2025-05-29 03:35:15 +0800

    WelcomePage fxml and controller added,
    when admin button clicks, it will navigate to login form admin
    
    also changes in pom file

--- src/main/java/com/mycompany/capsprojects/App.java
+++ src/main/java/com/mycompany/capsprojects/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("WelcomePage"), 1200, 768);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/mycompany/capsprojects/WelcomePageController.java
@@ -0,0 +1,26 @@
+package com.mycompany.capsprojects;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class WelcomePageController {
+    
+    @FXML
+    Button adminButton;
+    
+    @FXML
+    private void goToAdminPage() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginFormAdmin.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        
+        Stage stage = (Stage) adminButton.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+}
