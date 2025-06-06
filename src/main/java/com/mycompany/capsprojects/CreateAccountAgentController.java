4a2f14f2cd26667a01e92621d3b241192baabd2c
Markuuu13 <markjuscardeno13@gmail.com>
2025-06-06 18:47:09 +0800

    Create account agent and login agent done but no email validation

---
+++ src/main/java/com/mycompany/capsprojects/CreateAccountAgentController.java
@@ -0,0 +1,99 @@
+package com.mycompany.capsprojects;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+
+public class CreateAccountAgentController {
+    @FXML
+    TextField agentNameTxt, agentNumberTxt, emailAgentTxt, passwordAgentTxt, confirmPassAgentTxt;
+
+    String apiurl  = "http://localhost:4500/registerAgent";
+    @FXML
+    public void signUpUser() {
+        String name = agentNameTxt.getText();
+        String idnumber = agentNumberTxt.getText();
+        String email = emailAgentTxt.getText();
+        String password = passwordAgentTxt.getText();
+        String confirmPass = confirmPassAgentTxt.getText();
+
+        if (name.isBlank() || idnumber.isBlank() || email.isBlank() || password.isBlank()) {
+            showAlert(Alert.AlertType.ERROR, "Please fill in all fields");
+            return;
+        }
+        if (!password.equals(confirmPass)) {
+            showAlert(Alert.AlertType.ERROR, "Password doesn't match");
+            return;
+        }
+        agentRegisterService(name, idnumber, email, password);
+
+    }
+
+    public void agentRegisterService(String name, String idnumber, String email, String password) {
+        try {
+            String json = String.format(
+                    "{\"name\":\"%s\", \"id_number\":\"%s\", \"email\":\"%s\", \"password\":\"%s\"}",
+                    name, idnumber, email, password
+            );
+            URL url = new URL(apiurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type", "application/json");
+            
+            
+            try (OutputStream os = conn.getOutputStream()) {
+            byte[] input = json.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+            int status = conn.getResponseCode();
+            InputStream is = (status == 200) ? conn.getInputStream() : conn.getErrorStream();
+            
+            StringBuilder response = new StringBuilder();
+             
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(is, "utf-8"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                response.append(line.trim());
+            }
+        }
+            if(status == 200) {
+                showAlert(AlertType.CONFIRMATION, "Account Created Successfully");
+            }
+            else if(status == 400){
+                showAlert(AlertType.ERROR, "ID number not found or invalid");
+            }
+            else {
+                showAlert(AlertType.ERROR, "Error Occured");
+            }
+            System.out.println("Response from server: " + response.toString());
+            
+//           
+//            
+//            try(BufferedReader br = new BufferedReader(new InputStreamReader(is, "utf-8"))){
+//                
+//            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    private void showAlert(Alert.AlertType type, String message) {
+        Alert alert = new Alert(type);
+        alert.setTitle("Registration");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+}
+
--- src/main/java/com/mycompany/capsprojects/LoginFormAgentController.java
+++ src/main/java/com/mycompany/capsprojects/LoginFormAgentController.java
@@ -1,11 +1,19 @@
 package com.mycompany.capsprojects;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -17,16 +25,77 @@ public class LoginFormAgentController {
     private Text agentCreateAccount;
     @FXML
     private Text forgotPasswordAgent;
-    
     @FXML
-    private void goToDashboard() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("DashboardPage.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
+    TextField agentNumberTxt, passwordTxt;
+
+    String apiurl = "http://localhost:4500/loginAgent";
+    
+    // Signin User
+    @FXML
+    private void signIn() throws IOException {
+        String adminNumber = agentNumberTxt.getText();
+        String password = passwordTxt.getText();
+
+        if (adminNumber.isBlank() || password.isBlank()) {
+            showAlert(Alert.AlertType.ERROR, "All fields are required");
+            return;
+        }
         
-        Stage stage = (Stage) agentLoginButton.getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        // Pass the admin number and password
+        goToDashboard(adminNumber, password);
+    }
+    
+    // This method is for calling Login API
+    @FXML
+    private void goToDashboard(String agentNo, String pass) throws IOException {
+        try {
+            URL url = new URL(apiurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            conn.setRequestProperty("Content-Type", "application/json");
+            // This is a json format
+            String json = String.format(
+                    "{\"id_number\":\"%s\", \"password\":\"%s\"}",
+                    agentNo,pass);
+            
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = json.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+            
+            // Get the status code
+            int status = conn.getResponseCode();
+            InputStream is = (status == 200) ? conn.getInputStream() : conn.getErrorStream();
+
+            StringBuilder response = new StringBuilder();
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(is, "utf-8"))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    response.append(line.trim());
+                }
+            }
+            
+            if(status == 200) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("DashboardPage.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+
+            Stage stage = (Stage) agentLoginButton.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+            }
+            else {
+                showAlert(Alert.AlertType.ERROR, "Invalid ID Number or Password. Please try again.");
+            }
+        } catch (Exception ex) {
+                showAlert(Alert.AlertType.ERROR, "Error occured. Please try again.");
+        }
+        
     }
     @FXML
     private void goToAgentSignIn() throws IOException {
@@ -47,6 +116,14 @@ public class LoginFormAgentController {
         Stage stage = (Stage) forgotPasswordAgent.getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+    }
+    
+    private void showAlert(Alert.AlertType type, String message) {
+        Alert alert = new Alert(type);
+        alert.setTitle("Log in");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
 
